8b3f428e8dc465fdcde578f54f04092808dcb983
l33th <l33th@protonmail.ch>
2021-10-27 07:18:25 +0200

    added apiConfig file

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+const apiConfig = {
+	baseUrl: 'https://api.themoviedb.org/3/',
+	apiKey: 'e8ca87ad95dbb807127fc10aff0600dd',
+	originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+	w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+};
+
+export default apiConfig;
