d1b43ea93d6afffbae0cbea1d2da685003fcf66e
l33th <l33th@protonmail.ch>
2021-10-28 07:19:58 +0200

    added routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
+// import libraries
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+// import scss
 import './App.scss';
+
+// imports from react-router-dom
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// import components
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 
+import Routes from './config/Routes';
+
 function App() {
 	return (
-		<div className='App'>
-			<Header />
-			<h4>search for your favorite movies</h4>
-			<button className='btn'>search</button>
-
-			<div>
-				<Footer />
-			</div>
-		</div>
+		<BrowserRouter>
+			<Route
+				render={(props) => (
+					<>
+						<Header {...props} />
+						<Routes />
+						<Footer />
+					</>
+				)}
+			/>
+		</BrowserRouter>
 	);
 }
 
