a1d907ecf04bceee9cfbe9e50ebbb38deef4de17
l33th <l33th@protonmail.ch>
2021-10-28 07:35:47 +0200

    added useRef and useLocation constants

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 // import link from react-router-dom
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 // import scss
 import './header.scss';
 import logo from '../../assets/img/flixit.png';
@@ -23,8 +23,13 @@ const headerNav = [
 
 // the header functional component
 const Header = () => {
+	const { pathname } = useLocation();
+	const headerRef = useRef(null);
+
+	const active = headerNav.findIndex((e) => e.path === pathname);
+
 	return (
-		<div>
+		<div ref={headerRef} className='header'>
 			<h1>Flixit Movies</h1>
 		</div>
 	);
