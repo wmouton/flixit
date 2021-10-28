426a84961bb043d5eb0687c4c761832987fe17a0
l33th <l33th@protonmail.ch>
2021-10-28 07:32:21 +0200

    header nav setup with link import

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+// import link from react-router-dom
+import { Link } from 'react-router-dom';
 // import scss
 import './header.scss';
-import logo from '../../assets/img/flixit.jpg';
+import logo from '../../assets/img/flixit.png';
+
+// header nav object
+const headerNav = [
+	{
+		display: 'Home',
+		path: '/',
+	},
+	{
+		display: 'Movies',
+		path: '/movies',
+	},
+	{
+		display: 'TV Series',
+		path: '/tv',
+	},
+];
 
 // the header functional component
 const Header = () => {
