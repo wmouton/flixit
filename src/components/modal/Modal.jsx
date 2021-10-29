65b0cfd3da8f11c7dc214b855cdb458beed01811
l33th <l33th@protonmail.ch>
2021-10-29 14:50:58 +0200

    modal component jsx and styling

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+// import prop types from 'prop-types
+import PropTypes from 'prop-types';
 
-const Modal = () => {
+// modal functional component
+const Modal = (props) => {
+	const [active, setActive] = useState(false);
+
+	useEffect(() => {
+		setActive(props.active);
+	}, [props.active]);
+
 	return (
-		<div>
-			<h6>modal</h6>
+		<div id={props.id} className={`modal ${active ? 'active' : ''}`}>
+			{props.children}
 		</div>
 	);
+};
+
+Modal.propTypes = {
+	active: PropTypes.bool,
+	id: PropTypes.string,
+};
+
+export const ModalContent = (props) => {
+	const contentRef = useRef(null);
+
+	const closeModal = () => {
+		contentRef.current.parentNode.classList.remove('active');
+		if (props.onClose) props.onClose();
+	};
+
+	return (
+		<div ref={contentRef} className='modal__content'>
+			{props.children}
+			<div className='modal__content__close' onClick={closeModal}>
+				<i className='bx bx-x'></i>
+			</div>
+		</div>
+	);
+};
+
+ModalContent.propTypes = {
+	onClose: PropTypes.func,
 };
 
 export default Modal;
