bde465bc546ad4b5149c7bffbb96e66e4b598ecf
l33th <l33th@protonmail.ch>
2021-10-31 07:42:58 +0200

    movie card setup

---
+++ src/components/movie-card/MovieCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieCard = () => {
+	return (
+		<div>
+			<h6>Movie Card Setup</h6>
+		</div>
+	);
+};
+
+export default MovieCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,6 +23,15 @@ const Home = () => {
 					</div>
 					<MovieList category={category.movie} type={movieType.popular} />
 				</div>
+				<div className='section mb-3'>
+					<div className='section__header mb-2'>
+						<h2>Top Rated Movies</h2>
+						<Link to='/movie'>
+							<OutlineButton className='small'>View more</OutlineButton>
+						</Link>
+					</div>
+					<MovieList category={category.movie} type={movieType.top_rated} />
+				</div>
 			</div>
 		</>
 	);
