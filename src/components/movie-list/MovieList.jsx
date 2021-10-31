43e85580d9aa4499f659426e202926826389281d
l33th <l33th@protonmail.ch>
2021-10-31 07:20:34 +0200

    movie list setup

---
+++ src/components/movie-list/MovieList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieList = () => {
+	return (
+		<div>
+			<h6>Movie List Setup</h6>
+		</div>
+	);
+};
+
+export default MovieList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+// import link from react-router-dom
+import { Link } from 'react-router-dom';
 // import components
 import HeroSlide from '../components/hero-slide/HeroSlide';
+import MovieList from '../components/movie-list/MovieList';
+import { OutlineButton } from '../components/button/Button';
+// import from tmdbApi
+import { category, movieType, tvType } from '../api/tmdbApi';
 
 // the home functional component
 const Home = () => {
 	return (
 		<>
 			<HeroSlide />
+			<div className='container'>
+				<div className='section mb-3'>
+					<div className='section__header mb-2'>
+						<h2>Trending Movies</h2>
+						<Link to='/movie'>
+							<OutlineButton className='small'>View more</OutlineButton>
+						</Link>
+					</div>
+					<MovieList category={category.movie} type={movieType.popular} />
+				</div>
+			</div>
 		</>
 	);
 };
