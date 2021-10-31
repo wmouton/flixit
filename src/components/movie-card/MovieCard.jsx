763d5fdab31bef537d6652aac831a0c08b5c8b31
l33th <l33th@protonmail.ch>
2021-10-31 08:02:13 +0200

    display movie list

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+// import css
+import './movie-card.scss';
+// import link from react-router-dom
+import { Link } from 'react-router-dom';
+// import components
+import Button from '../button/Button';
+import { category } from '../../api/tmdbApi';
+import apiConfig from '../../api/apiConfig';
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+	const item = props.item;
+
+	const link = '/' + category[props.category] + '/' + item.id;
+
+	const bg = apiConfig.w500Image(item.poster_path || item.backdrop_path);
+
 	return (
-		<div>
-			<h6>Movie Card Setup</h6>
-		</div>
+		<Link to={link}>
+			<div className='movie-card' style={{ backgroundImage: `url(${bg})` }}>
+				<Button>
+					<i className='bx bx-play'></i>
+				</Button>
+			</div>
+			<h3>{item.title || item.name}</h3>
+		</Link>
 	);
 };
 
--- src/components/movie-list/MovieList.jsx
+++ src/components/movie-list/MovieList.jsx
@@ -11,7 +11,7 @@ import { Link } from 'react-router-dom';
 import Button from '../button/Button';
 import tmdbApi, { category } from '../../api/tmdbApi';
 import apiConfig from '../../api/apiConfig';
-// import MovieCard from './movie-card/MovieCard';
+import MovieCard from '../movie-card/MovieCard';
 
 const MovieList = (props) => {
 	const [items, setItems] = useState([]);
@@ -42,7 +42,7 @@ const MovieList = (props) => {
 			<Swiper grabCursor={true} spaceBetween={10} slidesPerView={'auto'}>
 				{items.map((item, i) => (
 					<SwiperSlide key={i}>
-						{/* <MovieCard item={item} category={props.category} /> */}
+						<MovieCard item={item} category={props.category} />
 					</SwiperSlide>
 				))}
 			</Swiper>
