418533281829f4461d37a4f443956985830fc210
l33th <l33th@protonmail.ch>
2021-11-01 13:06:53 +0200

    video list with state and useEffect

--- src/pages/detail/VideoList.jsx
+++ src/pages/detail/VideoList.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+// import from react-router
+import { useParams } from 'react-router';
+// import from API
+import tmdbApi from '../../api/tmdbApi';
 
-const VideoList = () => {
+// video list functional component
+const VideoList = (props) => {
+	const { category } = useParams();
+
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		const getVideos = async () => {
+			const res = await tmdbApi.getVideos(category, props.id);
+			setVideos(res.results.slice(0, 5));
+		};
+		getVideos();
+	}, [category, props.id]);
+
 	return (
-		<div>
-			<h6>video list</h6>
-		</div>
+		<>
+			{videos.map((item, i) => (
+				<Video key={i} item={item} />
+			))}
+		</>
 	);
 };
 
