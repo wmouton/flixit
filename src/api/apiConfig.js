be63c1a38570c81d305cfc1c1cfd1059ba1e957d
l33th <l33th@protonmail.ch>
2021-11-01 08:37:21 +0200

    footer content styling

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -3,7 +3,7 @@
 const apiConfig = {
 	baseUrl: 'https://api.themoviedb.org/3/',
 	// apiKey: API_KEY,
-	apiKey: '',
+	apiKey: 'e8ca87ad95dbb807127fc10aff0600dd',
 	originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
 	w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
