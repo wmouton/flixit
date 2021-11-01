eb347f2a5c555f99b984658dc6b21b1ac9bb3b21
l33th <l33th@protonmail.ch>
2021-11-01 10:04:54 +0200

    added detail styling

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -5,19 +5,40 @@ import { useParams } from 'react-router';
 import tmdbApi from '../../api/tmdbApi';
 import apiConfig from '../../api/apiConfig';
 // import css
-import './detail.scss';
+// import './detail.scss';
 // import components
-import CastList from './CastList';
-import VideoList from './VideoList';
-import MovieList from '../../components/movie-list/MovieList';
-
+// import CastList from './CastList';
+// import VideoList from './VideoList';
+// import MovieList from '../../components/movie-list/MovieList';
 
 // the detail functional component
 const Detail = () => {
+	const { category, id } = useParams();
+
+	const [item, setItem] = useState(null);
+
+	useEffect(() => {
+		const getDetail = async () => {
+			const response = await tmdbApi.detail(category, id, { params: {} });
+			setItem(response);
+			window.scrollTo(0, 0);
+		};
+		getDetail();
+	}, [category, id]);
+
 	return (
-		<div>
-			<h6>Detail</h6>
-		</div>
+		<>
+			{item && (
+				<div
+					className='banner'
+					style={{
+						backgroundImage: `url(${apiConfig.originalImage(
+							item.backdrop_path || item.poster_path
+						)})`,
+					}}
+				></div>
+			)}
+		</>
 	);
 };
 
