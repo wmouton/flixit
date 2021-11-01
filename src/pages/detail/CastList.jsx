cb6bc406aac6c47e2bc7f25289b04c5accef0191
l33th <l33th@protonmail.ch>
2021-11-01 10:18:37 +0200

    cast list setup

---
+++ src/pages/detail/CastList.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+// imports from react-router
+import { useParams } from 'react-router';
+// imports from APIs
+import tmdbApi from '../../api/tmdbApi';
+import apiConfig from '../../api/apiConfig';
+
+// cast list functional component
+const CastList = () => {
+	return (
+		<div>
+			<h6>cast list setup</h6>
+		</div>
+	);
+};
+
+export default CastList;
