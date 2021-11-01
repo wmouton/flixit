5e611f16ead9601b4703a20a43540e47b9ec387e
l33th <l33th@protonmail.ch>
2021-11-01 09:17:42 +0200

    added input files and grid useEffect

--- src/components/movie-grid/MovieGrid.jsx
+++ src/components/movie-grid/MovieGrid.jsx
@@ -10,7 +10,39 @@ import Input from '../input/Input';
 // imports from the movie database API
 import tmdbApi, { category, movieType, tvType } from '../../api/tmdbApi';
 
-const MovieGrid = () => {
+const MovieGrid = (props) => {
+	const [items, setItems] = useState([]);
+
+	const [page, setPage] = useState(1);
+	const [totalPage, setTotalPage] = useState(0);
+
+	const { keyword } = useParams();
+
+	useEffect(() => {
+		const getList = async () => {
+			let response = null;
+			if (keyword === undefined) {
+				const params = {};
+				switch (props.category) {
+					case category.movie:
+						response = await tmdbApi.getMoviesList(movieType.upcoming, {
+							params,
+						});
+						break;
+					default:
+						response = await tmdbApi.getTvList(tvType.popular, { params });
+				}
+			} else {
+				const params = {
+					query: keyword,
+				};
+				response = await tmdbApi.search(props.category, { params });
+			}
+			setItems(response.results);
+			setTotalPage(response.total_pages);
+		};
+		getList();
+	}, [props.category, keyword]);
 	return (
 		<div>
 			<h6>movie grid setup</h6>
