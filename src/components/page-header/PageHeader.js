0e745465bc50c361fc1449e11741608e72578115
l33th <l33th@protonmail.ch>
2021-11-01 08:50:35 +0200

    page header setup

---
+++ src/components/page-header/PageHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageHeader = () => {
+  return (
+    <div>
+      <h6>page header</h6>
+    </div>
+  )
+}
+
+export default PageHeader
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 // the catalog functional component
 const Catalog = () => {
+	const { category } = useParams();
+
+	console.log(category);
+	
 	return (
 		<div>
 			<h6>Catalog</h6>
