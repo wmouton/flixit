0c0ef0da769af764a9bfa9a8ac32ed09ffd0171f
l33th <l33th@protonmail.ch>
2021-11-01 10:20:30 +0200

    added params state and useEffect

--- src/pages/detail/CastList.jsx
+++ src/pages/detail/CastList.jsx
@@ -6,7 +6,19 @@ import tmdbApi from '../../api/tmdbApi';
 import apiConfig from '../../api/apiConfig';
 
 // cast list functional component
-const CastList = () => {
+const CastList = (props) => {
+	const { category } = useParams();
+
+	const [casts, setCasts] = useState([]);
+
+	useEffect(() => {
+		const getCredits = async () => {
+			const res = await tmdbApi.credits(category, props.id);
+			setCasts(res.cast.slice(0, 5));
+		};
+		getCredits();
+	}, [category, props.id]);
+
 	return (
 		<div>
 			<h6>cast list setup</h6>
