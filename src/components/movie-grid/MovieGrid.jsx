56239b09c5de760e0a1afca92e60fec631ae44ed
l33th <l33th@protonmail.ch>
2021-11-01 09:11:19 +0200

    movie grid imports

--- src/components/movie-grid/MovieGrid.jsx
+++ src/components/movie-grid/MovieGrid.jsx
@@ -1,4 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+// imports from react-router
+import { useHistory, useParams } from 'react-router';
+// import css
+import './movie-grid.scss';
+// import components
+import MovieCard from '../movie-card/MovieCard';
+import Button, { OutlineButton } from '../button/Button';
+import Input from '../input/Input';
+// imports from the movie database API
+import tmdbApi, { category, movieType, tvType } from '../../api/tmdbApi';
 
 const MovieGrid = () => {
 	return (
