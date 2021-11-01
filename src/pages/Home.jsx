1cd2bd5c0b6c368896d62324b022f8b595c4f476
l33th <l33th@protonmail.ch>
2021-11-01 09:49:05 +0200

    some cleanup and home jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,6 +32,25 @@ const Home = () => {
 					</div>
 					<MovieList category={category.movie} type={movieType.top_rated} />
 				</div>
+				<div className='section mb-3'>
+					<div className='section__header mb-2'>
+						<h2>Trending TV</h2>
+						<Link to='/tv'>
+							<OutlineButton className='small'>View more</OutlineButton>
+						</Link>
+					</div>
+					<MovieList category={category.tv} type={tvType.popular} />
+				</div>
+
+				<div className='section mb-3'>
+					<div className='section__header mb-2'>
+						<h2>Top Rated TV</h2>
+						<Link to='/tv'>
+							<OutlineButton className='small'>View more</OutlineButton>
+						</Link>
+					</div>
+					<MovieList category={category.tv} type={tvType.top_rated} />
+				</div>
 			</div>
 		</>
 	);
