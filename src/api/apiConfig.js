063d5d39a227d90bd1e3d4b4eec768d63de0806f
l33th <l33th@protonmail.ch>
2021-11-09 06:42:13 +0200

    set up footer links pages

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,7 @@
 // configure the API
 const apiConfig = {
 	baseUrl: 'https://api.themoviedb.org/3/',
-	apiKey: 'e8ca87ad95dbb807127fc10aff0600dd',
+	apiKey: '',
 	originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
 	w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
