c4fa15abc811094b0950cc413760b149b71cc9f2
l33th <l33th@protonmail.ch>
2022-07-04 19:15:37 +0200

    refactor: Protect API key

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,9 +1,11 @@
 // configure the API
 require('dotenv').config();
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 const apiConfig = {
 	baseUrl: 'https://api.themoviedb.org/3/',
-	apiKey: '',
+	apiKey: API_KEY,
 	originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
 	w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
