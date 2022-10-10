db5133d36a02e87d59573d03cbd3563cd0c469e3
l33th <l33th@localhost.localdomain>
2022-10-10 10:05:50 +0200

    chore: Vector updates

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -3,8 +3,7 @@
 
 const apiConfig = {
 	baseUrl: 'https://api.themoviedb.org/3/',
-	// apiKey: process.env.REACT_APP_API_KEY,
-	apiKey: "f092457843bfd363b651eb7e476e7a4d",
+	apiKey: process.env.REACT_APP_API_KEY,
 	originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
 	w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
