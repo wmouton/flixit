d3fb77010e1d538ea88034152b45f2b90dbfb90e
l33th <l33th@localhost.localdomain>
2022-10-10 09:56:25 +0200

    chore: Updated logo and footer

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,9 +1,10 @@
 // configure the API
-require('dotenv').config();
+// require('dotenv').config();
 
 const apiConfig = {
 	baseUrl: 'https://api.themoviedb.org/3/',
-	apiKey: process.env.REACT_APP_API_KEY,
+	// apiKey: process.env.REACT_APP_API_KEY,
+	apiKey: "f092457843bfd363b651eb7e476e7a4d",
 	originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
 	w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.scss';
 import { Link } from 'react-router-dom';
 // import components
 import bg from '../../assets/img/footer-bg.jpg';
-import logo from '../../assets/img/flixit.png';
+import logo from '../../assets/img/flixit.svg';
 
 // footer functional component
 const Footer = () => {
@@ -16,12 +16,15 @@ const Footer = () => {
 						<Link to='/'>
 							<img src={logo} alt='' />
 							<h4>Movies & Series</h4>
-							<br />
 						</Link>
 					</div>
 				</div>
+				<center>
+					<h4>Developed by <a style={{color: '#3FC2FC'}} href="https://wmouton.vercel.app">WMouton</a><br /><div style={{fontSize: '12px'}}>In collaboration with <a style={{color: '#3FC2FC'}} href="https://github.com/5n0weirdo">Snoweirdo</a></div></h4>
+				</center>
+				<br />
 					<center>
-						<h4>Copyright © 2022, WMouton</h4>
+						<h4>Copyright © 2022, <a href="https://flixit.netlify.app">Flixit</a></h4>
 					</center>
 				{/* <div className='footer__content__menus'>
 					<div className='footer__content__menu'>
